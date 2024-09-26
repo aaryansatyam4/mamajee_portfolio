09f8a585b73ca8b26c70cdadadbbcf3034cb35dd
aaryansatyam4 <aaryansatyam4@gmail.com>
2024-09-26 15:33:50 +0530

    adsasd

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Aboutme from './Components/Aboutme/Aboutme'
 import CareerGraph from './Components/CareerGraph/CareerGraph'
 import CareerHighlights from './Components/CareerHighlights/CareerHighlights'
-import Project from './Components/Projoect/Project'
+import Project from './Components/Project/Project'
 import Certifications from './Components/Certifications/Certifications'
 
 import Footer from './Components/Footer/Footer'
